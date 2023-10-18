be55aac55e99b0ae5426af2a0942541287458d6a
Klevis Cipi <cipiklevis@gmail.com>
2023-10-18 10:33:12 +0200

    Import for mysql driver

--- import.sh
+++ import.sh
@@ -1,4 +1,5 @@
 ENV=$1
+FILE_PATH=$2
 #local,staging,production
 
 DAY=$(date +"%d")
@@ -22,32 +23,24 @@ export USERNAME
 CNF_CONF_FILE=$(pwd)/credentials/.m-$ENV.cnf
 envsubst < $(pwd)/templates/.$DRIVER.cnf.template > $CNF_CONF_FILE
 
-unset PASSWORD
 unset DATABASE
 unset HOST
 unset PORT
 unset USERNAME
 
-DB=$DATABASE
+FILE=$IMPORT_FILE_PATH
 
-FILE="$DB.sql"
-
-DAY_BACK=$((DAY-1));
-MONTH_BACK=$((MONTH-1));
-YEAR_BACK=$((YEAR-1))
-
-if [ -d "$BACKUP_DIR_PATH/$YEAR$MONTH$DAY_BACK" ];then
-   rm -r $BACKUP_DIR_PATH/$YEAR$MONTH$DAY_BACK;
+if [ -n "$FILE_PATH" ];then
+    FILE=$FILE_PATH
 fi
 
-mkdir -p $BACKUP_DIR_PATH/$DATE
-
 if [ $DRIVER == "mysql" ];then
-mysqldump --defaults-extra-file=$CNF_CONF_FILE $DB > $BACKUP_DIR_PATH/$DATE/$FILE
+mysql --defaults-extra-file=$CNF_CONF_FILE $DATABASE < $FILE
 elif [ $DRIVER == "postgres" ];then
-pg_dump --file=$BACKUP_DIR_PATH/$DATE/$FILE --dbname=$CNF_CONF_FILE
+    echo "Driver=$DRIVER";
+    echo "In progress";
 fi
 
 rm $CNF_CONF_FILE
 
-echo "$DATE Ended..... ON=$ENV"
+echo "Ending Import....."
